1e6ccccbde2d6819a8b850ad1c72e280a084c0c6
German <me@g3rm4n.xyz>
2018-02-11 21:27:52 -0600

    🐛 FIX: Travis commit release

--- scripts/sentry.sh
+++ scripts/sentry.sh
@@ -1,15 +1,16 @@
 # Create a new release
+echo "$1"
 curl https://sentry.io/api/0/organizations/g3rm4n/releases/ \
   -X POST \
-  -H 'Authorization: Bearer ${SENTRY_TOKEN}' \
+  -H "Authorization: Bearer $1" \
   -H 'Content-Type: application/json' \
-  -d '
-  {
-    "version": "${TRAVIS_COMMIT}",
-    "refs": [{
-        "repository":"imamonsta/toc-toc",
-        "commit":"${TRAVIS_COMMIT}",
+  -d "
+{
+    \"version\": \"$2\",
+    \"refs\": [{
+        \"repository\":\"imamonsta/toc-toc\",
+        \"commit\":\"$2\"
     }],
-    "projects":["TocToc"]
+    \"projects\":[\"toctoc\"]
 }
-'
+"
