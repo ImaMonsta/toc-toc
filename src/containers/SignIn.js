925747b4a4ea73ff3b4f3d7504b5e870a3a02d02
German <me@g3rm4n.xyz>
2018-02-23 22:40:59 -0600

    📦 IMPROVE: update react-scripts

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -5,10 +5,12 @@ import Login from '../components/Login';
 class SignIn extends Component {
     render() {
         const { dispatch } = this.props;
-        return ([
-            <Banner key="Banner" title="Sign In" detail="Let's find new opportunities" search={false} />,
-            <Login key="Login" dispatch={dispatch} />,
-        ]);
+        return (
+            <React.Fragment>
+                <Banner title="Sign In" detail="Let's find new opportunities" search={false} />,
+                <Login dispatch={dispatch} />,
+            </React.Fragment>
+        );
     }
 }
 
